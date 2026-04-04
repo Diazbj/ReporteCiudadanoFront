caf2f92b9651436bb4a73f75810ea8adb0ef7449
diazj <diaz.jordyb@gmail.com>
2026-04-04 17:44:11 -0500

    tarea: cambiar apiUrl a ruta relativa en los archivos de configuración de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://reporte-ciudadano.azurewebsites.net/api'
+  apiUrl: '/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://reporte-ciudadano.azurewebsites.net/api'
+  apiUrl: '/api'
 };
