acc370b97cada5eda8e7b30d418879753cad8c1c
diazj <diaz.jordyb@gmail.com>
2026-05-04 15:02:16 -0500

    chore: untrack environment files and inject mapbox token in CI

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: '/api',
-  mapboxToken: 'TU_TOKEN_AQUI'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: '/api',
-  mapboxToken: 'TU_TOKEN_AQUI'
-};
