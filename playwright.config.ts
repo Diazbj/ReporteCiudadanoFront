661b90e8c823d4cab09c2484dcde5bb376a6592b
diazj <diaz.jordyb@gmail.com>
2026-05-04 20:33:37 -0500

    ajuste playwr

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,10 +6,9 @@ export default defineConfig({
   forbidOnly: !!process.env['CI'],
   retries: process.env['CI'] ? 2 : 0,
   workers: process.env['CI'] ? 1 : undefined,
-  reporter: [
-    ['html'],
-    ['playwright-testrail-reporter']
-  ],
+  reporter: process.env['CI']
+    ? [['html'], ['playwright-testrail-reporter']]
+    : [['html']],
   use: {
     baseURL: process.env['BASE_URL'] || 'http://localhost:4200',
     trace: 'on-first-retry',
